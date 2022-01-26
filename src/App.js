89176f1cb7e3e3094fb8138b441723b864ac2da2
Sara Bolduc <sarabolduc@Saras-MacBook-Pro.local>
2022-01-26 14:36:52 -0800

    add sign in button, not functional yet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import SignIn from './Components/SignIn';
 import About from './Components/About';
+
 
 function App() {
   return (
     <>
+    <SignIn />
     <About />
     </>
   );
---
+++ src/Components/SignIn/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function SignIn(){
+    return(
+        <>
+            <div id="login-btn" className="login-btn">
+                <button type="button" className="btn btn-outline-info login">Login</button>
+            </div>
+        </>
+    );
+}
+export default SignIn;
