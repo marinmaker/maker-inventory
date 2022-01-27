6e9eb369d281b776bd6ef657d44ebeca8948cee7
Sara Bolduc <sarabolduc@Saras-MacBook-Pro.local>
2022-01-27 13:47:35 -0800

    test btns on dashboard page

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,20 +1,28 @@
 import React, {useContext, useState, useEffect} from "react";
 import {Redirect} from "react-router-dom";
 
+import API from "../../utils/API";
+
 import {KitContext} from "../../context/KitContext";
 import {KitQuantityContext} from "../../context/KitQuantityContext";
-import {UsernameContext} from "../../context/UsernameContext"
-import { UserIdContext } from "../../context/UserIdContext"
+import {UsernameContext} from "../../context/UsernameContext";
+import { UserIdContext } from "../../context/UserIdContext";
 
 import Table from "../../Components/Table";
 
 const Items = () => {
-    const [redirect, setRedirect] = useState(null);
+    // const [redirect, setRedirect] = useState(null);
 
-    const [formData, setFormData] = useState({
-        title: "",
-        quantity:"",
-    });
+    // const [formData, setFormData] = useState({
+    //     title: "",
+    //     quantity:"",
+    // });
 
-    const {kits, setKits} = useContext()
-}
+    // const {kits, setKits} = useContext()
+
+    return(
+        <Table />
+    );
+};
+
+export default Items;
