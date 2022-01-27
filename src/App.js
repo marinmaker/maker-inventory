9132f2c6a6a243605ad0c60af044957f7ff7aa81
Sara Bolduc <sarabolduc@Saras-MacBook-Pro.local>
2022-01-26 16:05:00 -0800

    set up pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import React, {useState, UseContext} from 'react';
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
-import SignIn from './Components/SignIn';
-import About from './Components/About';
-import Edit from './Components/Edit';
+import Home from './pages/Home/Home';
+import { KitContext } from "./context/KitContext";
+import { KitQuantityContext } from "./context/KitQuantityContext";
+import { useState } from 'react';
 
 
 function App() {
-  return (
-    <>
-    <SignIn />
-    <About />
-    <Edit />
-    </>
+  const [kits, setKits] = useState([]);
+  const [kitQuantity, setKitQuantity] = useState();
+
+  return(
+   <KitQuantityContext.Provider value={{kitQuantity, setKitQuantity}}>
+     <KitContext.Provider value ={{kits, setKits}}>
+       <Router>
+         <Switch>
+           <Route exact path="/" component={Home}/>
+           <Route exact path="/dashboard" component={Dashboard} />
+         </Switch>
+       </Router>
+     </KitContext.Provider>
+   </KitQuantityContext.Provider>
   );
 }
 
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SignIn from './Components/SignIn';
+import About from './Components/About';
+import Edit from './Components/Edit';
+
+const Home = () => {
+     return (
+    <>
+    <SignIn />
+    <About />
+    <Edit />
+    </>
+  );
+}
+export default Home;
